3e5b35babdb351e74ef70b92a5c9d4235de5f263
Vlad Povarna <povarna@adobe.com>
2021-09-05 22:35:21 +0300

    ClimbingStairs Scala

---
+++ src/main/scala/com/leetcode/top100LikedQuestions/ClimbingStairs.scala
@@ -0,0 +1,31 @@
+package com.leetcode.top100LikedQuestions
+
+import scala.annotation.tailrec
+
+/**
+ * @author Adobe Systems Incorporated.
+ */
+object ClimbingStairs extends App {
+
+  def climbStairs(n: Int): Int = {
+    if (n == 1) 1
+    else if (n == 2) 2
+    else {
+      calculateResultRecursion(n, 1, 2, 3)
+    }
+  }
+
+  @tailrec
+  def calculateResultRecursion(n: Int, first: Int, second: Int, result: Int): Int = {
+    if (n < 3) result
+    else {
+      val newResult = first + second
+      calculateResultRecursion(n - 1, second, newResult, newResult)
+    }
+  }
+
+  println(climbStairs(2))
+  println(climbStairs(4))
+  println(climbStairs(5))
+
+}
