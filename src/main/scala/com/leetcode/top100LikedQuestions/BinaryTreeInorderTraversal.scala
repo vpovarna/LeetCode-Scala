c8bb322ac28ea4a8790289f10ff8bf9af7527127
Vlad Povarna <povarna@adobe.com>
2021-09-07 22:09:26 +0300

    Binary Tree Inode Traversal

--- src/main/scala/com/leetcode/top100LikedQuestions/BinaryTreeInorderTraversal.scala
+++ src/main/scala/com/leetcode/top100LikedQuestions/BinaryTreeInorderTraversal.scala
@@ -8,14 +8,14 @@ import scala.language.postfixOps
 object BinaryTreeInorderTraversal extends App {
 
   def inorderTraversal(root: TreeNode): List[Int] = {
-    if(root == null) return Nil
+    if(root == null)  Nil
     else inorderTraversal(root.left) ::: List(root.value) ::: inorderTraversal(root.right)
   }
 
   class TreeNode(var _value: Int) {
     var value: Int = _value
     var left: TreeNode = null
-    val right: TreeNode = null
+    var right: TreeNode = null
   }
 
 
