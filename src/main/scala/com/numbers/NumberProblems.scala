b684c76955e6f4bcd7cd33899ebefd07eacd9d66
Vlad Povarna <povarna@adobe.com>
2021-12-18 22:45:50 +0200

    Adventure of code problems

--- src/main/scala/com/numbers/NumberProblems.scala
+++ src/main/scala/com/numbers/NumberProblems.scala
@@ -16,11 +16,28 @@ class NumberProblems {
     if (n == 0 || n == 1 || n == -1) false
     else isPrimeTailRec(2)
   }
+
+  def decompose(n: Int): List[Int] = {
+    assert(n > 0)
+
+    @tailrec
+    def decomposeTailRec(remaining: Int, currentDivisor: Int, accumulator: List[Int]): List[Int] = {
+      if (currentDivisor > Math.sqrt(n)) remaining :: accumulator
+      else if (remaining % currentDivisor == 0) decomposeTailRec(remaining / currentDivisor, currentDivisor, currentDivisor :: accumulator)
+      else decomposeTailRec(remaining, currentDivisor + 1, accumulator)
+    }
+
+    decomposeTailRec(n, 2, List.empty)
+  }
 }
 
 object NumberProblems extends App {
   val numberProblems = new NumberProblems
+
   println(numberProblems.isPrime(11))
+  println(numberProblems.decompose(11))
+  println(numberProblems.decompose(15))
+  println(numberProblems.decompose(8))
 }
 
 class NumberProblemsSpec extends AnyWordSpec {
