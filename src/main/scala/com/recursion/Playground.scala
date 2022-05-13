c63f7cc34ca105c0472dd4bb67bec0830a20f1c0
Vlad Povarna <povarna@adobe.com>
2022-05-13 14:52:08 +0300

    Playgrond

--- src/main/scala/com/recursion/Playground.scala
+++ src/main/scala/com/recursion/Playground.scala
@@ -9,20 +9,41 @@ object Playground {
   }
 
   // Fibonacci
-  def fibonacci(n: Int):Int = {
+  def fibonacci(n: Int): Int = {
     if (n < 2) n
-    else fibonacci(n -1) + fibonacci(n-2)
+    else fibonacci(n - 1) + fibonacci(n - 2)
   }
 
-  // reverse word
+  // Reverse word
   def reverseStr(s: String): String = {
     if (s.isEmpty) ""
     else reverseStr(s.tail) + s.head
   }
 
+  // Fibonacci with Memoization
+  def fibMemo(n: Int): Int = {
+    var cache: Map[Int, Int] = Map.empty[Int, Int]
+
+    def fibonacci(n: Int): Int = {
+      if (cache.contains(n)) {
+        cache(n)
+      } else {
+        if (n < 2) n
+        else {
+          cache = cache.updated(n, fibonacci(n - 1) + fibonacci(n - 2))
+          cache(n)
+        }
+      }
+    }
+
+    fibonacci(n)
+  }
+
+
   def main(args: Array[String]): Unit = {
     println(factorialRecursive(5))
     println(fibonacci(8))
+    println(fibMemo(8))
     println(reverseStr("training"))
   }
 
