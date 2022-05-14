e9a705e75b4250e289a27ed5febede0857f1aefc
Vlad Povarna <povarna@adobe.com>
2022-05-14 12:17:32 +0300

    HouseRobber problem ADDENDUM

--- src/main/scala/com/leetcode/medium/HouseRobber.scala
+++ src/main/scala/com/leetcode/medium/HouseRobber.scala
@@ -4,14 +4,20 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 object HouseRobber {
-
+  // Dynamic programming
   def solution(nums: Array[Int]): Int = {
-    nums.foldLeft(0, 0) { (r, n) =>
-      val newMax = (r._1 + n) max r._2
-      (r._2, newMax)
-    }._2
-  }
+    if (nums.isEmpty) 0
+    else {
+      val dp: Array[Int] = new Array[Int](nums.length + 1)
+      dp.update(0, 0)
+      dp.update(1, nums(0))
 
+      (1 until nums.length).foreach { i =>
+        dp.update(i + 1, Math.max(dp(i), dp(i - 1) + nums(i)))
+      }
+      dp.last
+    }
+  }
 }
 
 class HouseRobberSpec extends AnyFlatSpec with Matchers {
@@ -20,6 +26,6 @@ class HouseRobberSpec extends AnyFlatSpec with Matchers {
     "return the max sum he can get from non consecutive houses" in {
     HouseRobber.solution(Array(1, 2, 3, 1)) should equal(4)
     HouseRobber.solution(Array(2, 7, 9, 3, 1)) should equal(12)
-    HouseRobber.solution(Array(2, 1, 1, 2)) should equal(4) // Not works
+    HouseRobber.solution(Array(2, 1, 1, 2)) should equal(4)
   }
 }
