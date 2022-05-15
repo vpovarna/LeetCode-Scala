b47a82a71e86b4381493026249c92157e34d24eb
Vlad Povarna <povarna@adobe.com>
2022-05-15 13:27:32 +0300

    HouseRobber problem using ArrayBuffer

--- src/main/scala/com/leetcode/medium/HouseRobber.scala
+++ src/main/scala/com/leetcode/medium/HouseRobber.scala
@@ -3,6 +3,8 @@ package com.leetcode.medium
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
+import scala.collection.mutable.ArrayBuffer
+
 object HouseRobber {
   // Dynamic programming
   def solution(nums: Array[Int]): Int = {
@@ -18,14 +20,28 @@ object HouseRobber {
       dp.last
     }
   }
+
+  def solutionV2(nums: Array[Int]): Int = {
+    if (nums.isEmpty) 0
+    else {
+      val dp: ArrayBuffer[Int] = ArrayBuffer.empty[Int]
+      dp.addOne(0)
+      dp.addOne(nums(0))
+      (1 until nums.length).foreach { i =>
+        dp.addOne(Math.max(dp(dp.length - 1), dp(dp.length - 2) + nums(i)))
+      }
+      dp.last
+    }
+  }
 }
 
 class HouseRobberSpec extends AnyFlatSpec with Matchers {
 
   "House Robber" should
     "return the max sum he can get from non consecutive houses" in {
-    HouseRobber.solution(Array(1, 2, 3, 1)) should equal(4)
+    HouseRobber.solutionV2(Array(1, 2, 3, 1)) should equal(4)
     HouseRobber.solution(Array(2, 7, 9, 3, 1)) should equal(12)
-    HouseRobber.solution(Array(2, 1, 1, 2)) should equal(4)
+    HouseRobber.solutionV2(Array(2, 7, 9, 3, 1)) should equal(12)
+    HouseRobber.solutionV2(Array(2, 1, 1, 2)) should equal(4)
   }
 }
