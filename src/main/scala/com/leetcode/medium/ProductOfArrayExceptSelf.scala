6e2dcabc89c61482951c5c96c908b6c95824096c
Vlad Povarna <povarna@adobe.com>
2022-05-16 17:36:18 +0300

    ProductOfArrayExceptSelf problem ADDENDUM

--- src/main/scala/com/leetcode/medium/ProductOfArrayExceptSelf.scala
+++ src/main/scala/com/leetcode/medium/ProductOfArrayExceptSelf.scala
@@ -23,6 +23,24 @@ object ProductOfArrayExceptSelf {
     result
   }
 
+  def productExceptSelfV2(nums: Array[Int]): Array[Int] = {
+    val outputArray: Array[Int] = Array.fill[Int](nums.length)(1)
+
+    for (i <- 1 to nums.length - 1) {
+      outputArray.update(i, outputArray(i - 1) * nums(i - 1))
+    }
+    //    println(dp1.mkString("Array(", ", ", ")"))
+
+    var r: Int = 1;
+    for (i <- nums.length - 1 to 0 by -1) {
+      outputArray.update(i, outputArray(i) * r)
+      r = r * nums(i)
+    }
+    //    println(dp2.mkString("Array(", ", ", ")"))
+
+    outputArray
+  }
+
   def productExceptSelfFp(nums: Array[Int]): Array[Int] = {
      val dp1 = (1 until nums.length).scanLeft(1) { (prevValue, i) =>
         prevValue * nums(i - 1)
@@ -42,6 +60,9 @@ object ProductOfArrayExceptSelf {
 
     println(productExceptSelfFp(Array(4, 5, 1, 8, 2)).mkString("Array(", ", ", ")"))
     println(productExceptSelfFp(Array(1, 2, 3, 4)).mkString("Array(", ", ", ")"))
+
+    println(productExceptSelfV2(Array(4, 5, 1, 8, 2)).mkString("Array(", ", ", ")"))
+    println(productExceptSelfV2(Array(1, 2, 3, 4)).mkString("Array(", ", ", ")"))
   }
 
 }
