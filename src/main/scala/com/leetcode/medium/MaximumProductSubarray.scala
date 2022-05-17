22bc36fb4b6afd22b7697ab44ad175047895658e
Vlad Povarna <povarna@adobe.com>
2022-05-17 18:45:57 +0300

    Maximum Product Subarray problems DP solution

--- src/main/scala/com/leetcode/medium/MaximumProductSubarray.scala
+++ src/main/scala/com/leetcode/medium/MaximumProductSubarray.scala
@@ -3,6 +3,7 @@ package com.leetcode.medium
 import org.scalatest.wordspec.AnyWordSpec
 
 import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
@@ -10,6 +11,25 @@ import scala.annotation.tailrec
 class MaximumProductSubarray {
 
   def maxProduct(nums: Array[Int]): Int = {
+    val len = nums.length
+    val maxDp = Array.fill[Int](len)(0)
+    val minDp = Array.fill[Int](len)(0)
+
+    maxDp(0) = nums(0)
+    minDp(0) = nums(0)
+
+    for (i <- 1 until len) {
+      val fromMaxDp = maxDp(i - 1) * nums(i)
+      val fromMinDp = minDp(i - 1) * nums(i)
+      maxDp(i) = nums(i) max fromMaxDp max fromMinDp
+      minDp(i) = nums(i) min fromMaxDp min fromMinDp
+    }
+
+    maxDp.max
+  }
+
+
+  def maxProductV1(nums: Array[Int]): Int = {
     if (nums.length == 0) 0
     else {
       var maxEnding = nums(0)
@@ -24,6 +44,16 @@ class MaximumProductSubarray {
       }
       maxSoFar
     }
+  }
+}
+
+object MaximumProductSubarray {
+  def main(args: Array[String]): Unit = {
+    val productSubarray = new MaximumProductSubarray()
+    println(productSubarray.maxProduct(Array(2, 3, -2, 4))) // 6
+    println(productSubarray.maxProduct(Array[Int](0, 2))) // 2
+    println(productSubarray.maxProduct(Array[Int](-2, 0, -1))) // 0
+
   }
 }
 
