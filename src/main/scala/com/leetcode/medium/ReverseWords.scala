a72c3edea9977ff431ee6e6573e20f7f18b4be14
Vlad Povarna <povarna@adobe.com>
2022-11-13 11:27:06 +0200

    ReverseWordsInAString problem foldLeft impl

--- src/main/scala/com/leetcode/medium/ReverseWords.scala
+++ src/main/scala/com/leetcode/medium/ReverseWords.scala
@@ -6,10 +6,17 @@ object ReverseWords {
     s.split(" ").map(_.trim).filter(_.nonEmpty).reverse.mkString(" ").trim
   }
 
+  def reverseWords2(s: String): String = {
+    val words = s.split(" ")
+    (words.length - 1 to 0 by -1).foldRight("") { (i, sb) =>
+      if (words(i).nonEmpty) words(i).trim + " " + sb
+      else sb
+    }.trim
+  }
+
   def main(args: Array[String]): Unit = {
-    val inputStr = "  hello world  "
-    println(reverseWords(inputStr))
-    println(reverseWords("a good   example"))
-    println(reverseWords("the sky is blue"))
+    println(reverseWords2("  hello world  "))
+    println(reverseWords2("a good   example"))
+    println(reverseWords2("the sky is blue"))
   }
 }
